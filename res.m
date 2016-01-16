bc774b8c2fad2511316ede979f7b4b8fc094b41e
douglasjfm <douglasjfm@gmail.com>
2016-01-16 09:23:39 -0300

    melhora ploteer

--- res.m
+++ res.m
@@ -1,4 +1,4 @@
-exp = '_16';
+function res(exp)
 grafos(exp,8,0);
 grafos(exp,8,1);
 grafos(exp,8,2);
@@ -11,3 +11,4 @@ grafos(exp,32,2);
 grafos(exp,64,0);
 grafos(exp,64,1);
 grafos(exp,64,2);
+endfunction
--- scores.m
+++ scores.m
@@ -3,20 +3,39 @@ posi = 0;
 nega = 0;
 gn = 0;
 gp = 0;
-for i=1:length(spos)
-	if (spos(i) >= teta)
-		posi = 1 + posi;
-		gp = gp + 1;
-	else
-		gn = gn + 1;
-	endif
-endfor	
-for i=1:length(sneg)
-	if (sneg(i) < teta)
-		nega = 1 + nega;
-		gn = gn + 1;
-	else
-		gp = gp + 1;
-	endif
-endfor
+countp = 1:length(spos);
+countn = 1:length(sneg);
+pos2 = sort(spos,'descend');
+neg2 = sort(sneg);
+tetap = ones(1,length(spos));
+tetan = ones(1,length(sneg));
+tetap = tetap * teta;
+tetan = tetan * teta;
+boolp = pos2 >= tetap;
+booln = neg2 < tetan;
+countp = countp .* boolp;
+countn = countn .* booln;
+[~,posi] = max(countp);
+[~,nega] = max(countn);
+posi = posi * boolp(1);
+nega = nega * booln(1);
+gp = posi + length(sneg) - nega;
+gn = nega + length(spos) - posi;
+%for i=1:length(spos)
+%	if (spos(i) >= teta)
+%		posi = 1 + posi;
+%		gp = gp + 1;
+%	else
+%		gn = gn + 1;
+%	endif
+%endfor
+
+%for i=1:length(sneg)
+%	if (sneg(i) < teta)
+%		nega = 1 + nega;
+%		gn = gn + 1;
+%	else
+%		gp = gp + 1;
+%	endif
+%endfor
 endfunction
